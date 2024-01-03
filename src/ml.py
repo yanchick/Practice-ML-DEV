bd55180a92527075e2401c5d39765eb1637e63a7
Victor Barbarich <victor.barbarich@dif.tech>
2024-01-03 03:05:39 +0300

    Сделал наследование для моделей

--- src/ml.py
+++ src/ml.py
@@ -16,8 +16,9 @@ class ColumnConcater(BaseEstimator, TransformerMixin):
             ' Merchant_' + X['Merchant ID'].astype(str)
 
 
-class CategoryPredictor:
-    def __init__(self, model_path: str = '../models/model__logreg__count_min_df_20_v1.joblib'):
+class BasePredictor:
+
+    def __init__(self, model_path: str):
         self.model = joblib.load(model_path)
 
     def predict(self, data: pd.DataFrame):
@@ -25,8 +26,68 @@ class CategoryPredictor:
         return self.model.predict(data)
 
 
+class CategoryPredictorV001(BasePredictor):
+    """
+    "Model_001": {
+        "model": "LogisticRegression",
+        "vectorizer": "CountVectorizer",
+        "params": "min_df=20, max_iter=1000",
+        "metrics": {
+            "cv_accuracy": 0.952,
+            "accuracy": 0.97
+        },
+        "time_prediction_mac_m2": {
+            "mean": 0.0004
+        }
+    }
+    """
+
+    def __init__(self, model_path: str = '../models/model__logreg__count_min_df_20_v1.joblib'):
+        super().__init__(model_path)
+
+
+class CategoryPredictorV002(BasePredictor):
+    """
+    "Model_002": {
+        "model": "RandomForestClassifier",
+        "vectorizer": "CountVectorizer",
+        "params": "min_df=20",
+        "metrics": {
+            "cv_accuracy": 0.942,
+            "accuracy": 0.9636
+        },
+        "time_prediction_mac_m2": {
+            "mean": 0.027
+        }
+    }
+    """
+
+    def __init__(self, model_path: str = '../models/model__forest__count_min_df_20_v1.joblib'):
+        super().__init__(model_path)
+
+
+class CategoryPredictorV003(BasePredictor):
+    """
+    "Model_003": {
+        "model": "DecisionTreeClassifier",
+        "vectorizer": "TfidfVectorizer",
+        "params": "default",
+        "metrics": {
+            "cv_accuracy": 0.923,
+            "accuracy": 0.981
+        },
+        "time_prediction_mac_m2": {
+            "mean": 0.0008
+        }
+    }
+    """
+
+    def __init__(self, model_path: str = '../models/model__tree__tfidf_v1.joblib'):
+        super().__init__(model_path)
+
+
 if __name__ == '__main__':
-    predictor = CategoryPredictor()
+    predictor = CategoryPredictorV003()
     cases = pd.DataFrame({
         ' Category ID': [1, 2, 3],
         ' Category Label': [
@@ -40,4 +101,5 @@ if __name__ == '__main__':
         'Product ID': [5, 5, 5],
         'Product Title': ['Mobile Phones', 'TVs', 'Fridges'],
     })
-    print(type(predictor.predict(cases)))  # 'Mobile Phones
+    print(predictor.predict(cases))  # ['Mobile Phones', 'TVs', 'Fridges']
+    print(type(predictor.predict(cases)))
