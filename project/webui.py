ae45c7bffe6e816fd69bacf9bc3c22e3a21ea5d6
Repellista <winx41@gmail.com>
2024-01-24 11:48:53 +0300

    more user-friendly

--- project/webui.py
+++ project/webui.py
@@ -9,18 +9,18 @@ app = Dash(__name__)
 # Define layout
 app.layout = html.Div(
     [
-        html.H1("FastAPI + Dash Web UI"),
-        dcc.Input(id="feature1", type="number", placeholder="Enter Feature 1"),
-        dcc.Input(id="feature2", type="number", placeholder="Enter Feature 2"),
-        dcc.Input(id="feature3", type="number", placeholder="Enter Feature 3"),
-        dcc.Input(id="feature4", type="number", placeholder="Enter Feature 4"),
-        dcc.Input(id="feature5", type="number", placeholder="Enter Feature 5"),
-        dcc.Input(id="feature6", type="number", placeholder="Enter Feature 6"),
-        dcc.Input(id="feature7", type="number", placeholder="Enter Feature 7"),
-        html.Button("Submit", id="submit-button"),
+        html.H1("Сервис по предсказанию возрастной группы"),
+        dcc.Input(id="feature1", type="number", placeholder="Введите RIAGENDR"),
+        dcc.Input(id="feature2", type="number", placeholder="Введите PAQ605"),
+        dcc.Input(id="feature3", type="number", placeholder="Введите BMXBMI"),
+        dcc.Input(id="feature4", type="number", placeholder="Введите LBXGLU"),
+        dcc.Input(id="feature5", type="number", placeholder="Введите DIQ010"),
+        dcc.Input(id="feature6", type="number", placeholder="Введите LBXGLT"),
+        dcc.Input(id="feature7", type="number", placeholder="Введите LBXIN"),
+        html.Button("Предсказать", id="submit-button"),
         html.Div(id="hidden-prediction-id", style={"display": "none"}),  # Hidden div to store prediction ID
-        dcc.Input(id="prediction-id", type="number", placeholder="Enter Prediction ID"),
-        html.Button("Get Prediction", id="get-prediction-button"),
+        dcc.Input(id="prediction-id", type="number", placeholder="Введите ID"),
+        html.Button("Результат", id="get-prediction-button"),
         html.Div(id="output-message"),
         html.Div(id="predictions-output")  # Add a new HTML div for displaying predictions
     ]
@@ -91,7 +91,7 @@ def make_and_get_predictions(
 
             # Return both the output message and the stored prediction ID
             return (
-                f"Data uploaded successfully. Prediction result: {prediction_result}. Prediction ID: {prediction_id}",
+                f"Данные успешно загружены. Выполняется предсказание. Чтобы получить результат, введите номер ID и нажмите кнопку 'Результат'. ID: {prediction_id}",
                 prediction_id,
                 None
             )
@@ -102,21 +102,28 @@ def make_and_get_predictions(
                 None
             )
 
+
     elif triggered_id == "get-prediction-button" and prediction_id is not None:
+
         # Call FastAPI endpoint to get predictions using the provided prediction ID
+
         predictions_response = requests.get(f"http://127.0.0.1:8000/get-prediction-result/{int(prediction_id)}")
 
         if predictions_response.status_code == 200:
-            # Update predictions if available
-            predictions = f"Predictions: {predictions_response.json()}"
+
+            # Extract the prediction result directly
+
+            prediction_result = predictions_response.json().get("prediction_result", "No prediction result")
+
+            predictions = f"Результат предсказания: {prediction_result}"
+
         else:
+
             predictions = f"Failed to fetch predictions. Status code: {predictions_response.status_code}"
 
         # Return the predictions
-        return None, None, predictions
 
-    # Return default values if no button is clicked
-    return None, None, None
+        return None, None, predictions
 
 if __name__ == "__main__":
     app.run_server(debug=True)
